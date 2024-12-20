d6bf4b76382b0167464a5efd837a752963aef57d
Bakhrom Usmanov <bakhromusmanov01@gmail.com>
2024-12-20 22:29:31 +0500

    Initial Commit

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Bakhrom Usmanov on 20/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
