d086948799757c4849b0379af9e825122f9f6d68
Bakhrom Usmanov <bakhromusmanov01@gmail.com>
2025-05-13 17:09:29 +0500

    fix: Resolve conflicts in AuthService.swift and ImageEndpoint.swift

---
+++ InstagramClone/Services/AuthService.swift
@@ -0,0 +1,68 @@
+//
+//  AuthService.swift
+//  InstagramClone
+//
+//  Created by Bakhrom Usmanov on 29/04/25.
+//
+
+import Foundation
+import FirebaseAuth
+import FirebaseDatabase
+
+struct AuthService {
+   
+   typealias AuthDataResultCallback = (AuthDataResult?, Error?) -> Void
+   
+   static var isUserLoggedOut: Bool {
+      return Auth.auth().currentUser == nil
+   }
+   
+   static func logout() {
+      do {
+         try Auth.auth().signOut()
+      } catch {
+         print("DEBUG: Error while signing out")
+      }
+   }
+   
+   static func login(withEmail email: String, password: String, completion: AuthDataResultCallback?) {
+      Auth.auth().signIn(withEmail: email, password: password, completion: completion)
+   }
+   
+   static func register(user: AuthEntity, completion: @escaping (Error?) -> Void) {
+      
+      var user = user
+      
+      Auth.auth().createUser(withEmail: user.email, password: user.password, completion: { result, error in
+         if let error {
+            completion(error)
+            return
+         }
+         
+         guard let uid = result?.user.uid else {
+            print("DEBUG: Error while generating UID")
+            return
+         }
+         
+         user.userId = uid
+         let userPath = UserEndpoint.user(uid: uid).path
+         let ref = Database.database().reference().child(userPath)
+         
+         do {
+            let data = try JSONEncoder().encode(user)
+            let dictionary = try JSONSerialization.jsonObject(with: data) as? [String : Any]
+            ref.setValue(dictionary) { error, _ in
+               completion(error)
+               return
+            }
+         } catch {
+            completion(error)
+            return
+         }
+         
+      })
+      
+   }
+   
+}
+
--- InstagramClone/Services/ImageEndpoint.swift
+++ InstagramClone/Services/ImageEndpoint.swift
@@ -1,9 +1,4 @@
-//
-<<<<<<<< HEAD:InstagramClone/Services/ImageEndpoint.swift
-//  ImageEndpoint.swift
-========
 //  UserEndpoint.swift
->>>>>>>> db16314fad2655da9e349401f91282d408965f30:InstagramClone/Services/UserEndpoint.swift
 //  InstagramClone
 //
 //  Created by Bakhrom Usmanov on 30/04/25.
@@ -11,19 +6,12 @@
 
 import Foundation
 
-<<<<<<<< HEAD:InstagramClone/Services/ImageEndpoint.swift
 enum ImageEndpoint {
-========
-enum UserEndpoint {
-   case user(uid: String)
->>>>>>>> db16314fad2655da9e349401f91282d408965f30:InstagramClone/Services/UserEndpoint.swift
    case profileImage
    case postImage
    
    var path: String {
       switch self {
-      case .user(let uid):
-         return "users/\(uid)"
       case .profileImage:
          let userId = "userId"
          return "/users\(userId)/images/profile_image.jpg"
